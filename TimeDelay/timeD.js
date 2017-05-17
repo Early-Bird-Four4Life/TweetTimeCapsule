ca7e2ff46b385d8b73309f6363f35c939362b0b3
Aman Ibrahim <amanmibra@gmail.com>
2017-05-17 14:41:05 -0700

    Added 'Barebone' Files

---
+++ TimeDelay/timeD.js
@@ -0,0 +1,19 @@
+function tweetObj(tweet, time){
+    this.tweet = tweet;
+    this.date = time;
+}
+
+$('#submit').click(function(){
+  var tweet_input = document.getElementById('tweet')
+  var mainDiv = document.getElementById('content')
+
+  , 
+  var timeDelay = document.getElementById('time').value * 1000
+  console.log("time delay: " + timeDelay + " ms")
+  window.setTimeout(function(){
+    var tweet_value = document.createElement('h1');
+    tweet_value.innerHTML = tweet_input.value;
+
+    mainDiv.appendChild(tweet_value);
+  }, timeDelay)
+});
